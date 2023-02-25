6c2b6bae1df4a6554a061f250210ef08886c65c3
Roberto <senyor.it@yandex.ru>
2023-02-25 20:28:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android_app/RegistrationActivity.java
@@ -0,0 +1,64 @@
+package com.example.android_app;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.android_app.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText NicknameTextField;
+
+    /**
+     * короткая вибрация (50мсек)
+     */
+    private void smallVibration() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            v.vibrate(50);
+        }
+    }
+
+    /**
+     * средняя вибрация (150мсек)
+     */
+    private void mediumVibration() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(150, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            v.vibrate(150);
+        }
+    }
+
+    /**
+     * длинная вибрация (200мсек)
+     */
+    private void largeVibration() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            v.vibrate(200);
+        }
+    }
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_page);
+
+        NicknameTextField = (EditText) findViewById(R.id.NicknameTextField);
+
+    }
+}
