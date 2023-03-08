d33e7b70668d1ae2507dd254d0f0c4ac26d33779
Roberto <senyor.it@yandex.ru>
2023-03-08 14:45:28 +0700

    Поменял фрагменты для удобной работы

---
+++ app/src/main/java/com/example/android_app/ui/Activities/PersonsFragment.java
@@ -0,0 +1,21 @@
+package com.example.android_app.ui.Activities;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android_app.R;
+
+public class PersonsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_persons, container, false);
+    }
+}
